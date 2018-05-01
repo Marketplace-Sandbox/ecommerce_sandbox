1516ead3d73bd4452591c9197860772a8eb70545
Shaikhah Maasher <sh.maasher@gmail.com>
2018-05-01 20:00:20 +0300

    test pull request

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ecommerce Sandbox
+                    Ecommerce Sandbox  
                 </div>
             </div>
         </div>
